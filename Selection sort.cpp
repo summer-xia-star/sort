0220cbfde112393d36393822557ef938d4645ad4
Tian Xia <79569187+summer-xia-star@users.noreply.github.com>
2021-12-01 13:27:23 +0800

    Update Selection sort.cpp

--- Selection sort.cpp
+++ Selection sort.cpp
@@ -1,1 +1,12 @@
-
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i < n-1; i ++)
+    {
+        int min = i;
+        for (int j = i+1; j < n; j ++)
+            if (arr[j] < arr[min])
+                min = j;
+        swap(arr[min], arr[i]);
+    }
+    return;
+}
